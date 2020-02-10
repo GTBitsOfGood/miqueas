b8d96aab03c1ad2235c559b4b0072a21a567a18c
Michael Ryan <michael_ryan_2000@yahoo.com>
2020-02-09 22:07:18 -0500

    Started work on form creation tool component in react

--- pages/inventory.js
+++ pages/inventory.js
@@ -1,9 +1,11 @@
 import InventoryHeader from '../components/InventoryHeader'
+import SmartForm from '../components/SmartForm';
 
 export default function Inventory() {
   return (
     <div>
       <InventoryHeader />
+      <SmartForm/>
     </div>
   );
 }
