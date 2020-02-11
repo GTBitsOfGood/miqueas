ecbaf51b85c8539c44082b55057a19938e35d821
Wong Kin Li <wongkinli@lawn-128-61-12-108.lawn.gatech.edu>
2020-02-10 19:46:51 -0500

    Create transaction model

--- models/transaction.js
+++ models/transaction.js
@@ -1,19 +1,19 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable indent */
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const TransactionSchema = new Schema({
-    property1: {
-        type: Number,
-        required: true
-    },
-    property2: {
-        type: String,
-        required: true,
-    }
+const transactionSchema = new Schema({
+    name: { type: Number, required: true },
+    category: { type: String, required: true },
+    boy_or_girl: String,
+    type_and_color: String,
+    size: String,
+    transaction_date: { type: Date, default: Date.now }, // Date object records the date/time down to millisecond      
+    location: { type: String, required: true },
+    staff_name: { type: String, required: true},
+    quantity_change: { type: Number, required: true },
+    recipient: String,
 });
 
-const Transaction = mongoose.model('Transaction', TransactionSchema);
+const Transaction = mongoose.model('Transaction', transactionSchema);
 module.exports = Transaction;
 
