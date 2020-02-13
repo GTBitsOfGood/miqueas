e05d4fec15d16557e95df87a3b27241cd6ff4ea6
Sho Szczepaniuk <sszczepaniuk3@gatech.edu>
2020-02-13 14:02:05 -0500

    Cleaned up css

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -8,11 +8,11 @@ import '../public/navbar.css';
 
 export default () => (
   <div>
-    <ActiveLink href="/profile"><Button className="inline-block"><Person size="48"/><span className ="caption">Profile</span></Button></ActiveLink>
-    <ActiveLink href="/inventory"><Button className="inline-block"><ShoppingCart size="48"/><span className ="caption">Shopping List</span></Button></ActiveLink>
-    <ActiveLink href="/add"><Button variant="secondary" className="inline-block"><Plus size="48"/><span className ="caption">Add Item</span></Button></ActiveLink>
-    <ActiveLink href="/log"><Button className="inline-block"><Clipboard size="48"/><span className ="caption">View Log</span></Button></ActiveLink>
-    <ActiveLink href="/"><Button className="inline-block"><Clock size="48"/><span className ="caption">Transaction History</span></Button></ActiveLink>
+    <ActiveLink href="/profile"><Button><Person size="48"/><span className ="caption">Profile</span></Button></ActiveLink>
+    <ActiveLink href="/inventory"><Button><ShoppingCart size="48"/><span className ="caption">Shopping List</span></Button></ActiveLink>
+    <ActiveLink href="/add"><Button variant="secondary"><Plus size="48"/><span className ="caption">Add Item</span></Button></ActiveLink>
+    <ActiveLink href="/log"><Button><Clipboard size="48"/><span className ="caption">View Log</span></Button></ActiveLink>
+    <ActiveLink href="/"><Button><Clock size="48"/><span className ="caption">Transaction History</span></Button></ActiveLink>
 
   </div>
 ) 
