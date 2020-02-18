b9b3f7f8a5e8eedfe6ca5eb1858f8012b0983c2c
llxxll12345 <charlieliu@lawn-128-61-43-42.lawn.gatech.edu>
2020-02-17 19:59:44 -0500

    mod

--- pages/qrcode.js
+++ pages/qrcode.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react'
 import QrReader from 'react-qr-reader'
+import { useMediaQuery } from 'react-responsive'
+
  
-class Test extends Component {
-  state = {
-    result: 'No result'
-  }
- 
-  handleScan = data => {
-    if (data) {
-      this.setState({
-        result: data
-      })
-      window.location.assign('/');
+
+const Test  = () => {
+    const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 1224 })
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 })
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+
+    const handleScan = (data) => {
+      if (data) {
+        console.log(data);
+        //window.location.assign('/');
+      }
     }
-  }
-  handleError = err => {
-    console.error(err)
-  }
-  render() {
+      
+    const handleError = err => {
+        console.error(err)
+    }
+
+    let winWidth = '70%'
+    if (isTablet) {
+      winWidth = '80%'
+    }
+    if (isDesktopOrLaptop) {
+      winWidth = '90%'
+    }
     return (
-      <div>
-        <QrReader
-          delay={300}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          style={{ width: '30%' }}
-        />
-        <p>{this.state.result}</p>
-      </div>
+        <div>
+            <QrReader
+              delay={300}
+              onError={handleError}
+              onScan={handleScan}
+              style={{ width: winWidth }}
+            />
+        </div>
     )
-  }
 }
+
 
 export default Test
