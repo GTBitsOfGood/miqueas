07fcaab37eb2b640e65c129fbf9fb9989cffc496
Sho Szczepaniuk <sszczepaniuk3@gatech.edu>
2020-02-18 00:28:11 -0500

    Navigation Bar is Responsive
    Made the bar responive and cleaned up the code a bit.

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,5 +1,5 @@
 import ActiveLink from './ActiveLink'
-import { Button} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 import {ShoppingCart, Person} from 'styled-icons/material'
 import {Plus} from 'styled-icons/boxicons-regular'
 import {Clipboard} from 'styled-icons/boxicons-regular'
@@ -8,11 +8,20 @@ import '../public/navbar.css';
 
 export default () => (
   <div>
-    <ActiveLink href="/profile"><Button><Person size="48"/><span className ="caption">Profile</span></Button></ActiveLink>
-    <ActiveLink href="/inventory"><Button><ShoppingCart size="48"/><span className ="caption">Shopping List</span></Button></ActiveLink>
-    <ActiveLink href="/add"><Button variant="secondary"><Plus size="48"/><span className ="caption">Add Item</span></Button></ActiveLink>
-    <ActiveLink href="/log"><Button><Clipboard size="48"/><span className ="caption">View Log</span></Button></ActiveLink>
-    <ActiveLink href="/"><Button><Clock size="48"/><span className ="caption">Transaction History</span></Button></ActiveLink>
-
+    <ActiveLink href="/profile">
+      <Button><Person size="30%"/><span className="caption">Profile</span></Button>
+      </ActiveLink>
+    <ActiveLink href="/inventory">
+      <Button><ShoppingCart size="30%"/><span className="caption">Shopping List</span></Button>
+      </ActiveLink>
+    <ActiveLink href="/add">
+      <Button variant="secondary"><Plus size="30%" /><div className="caption">Add Item</div></Button>
+      </ActiveLink>
+    <ActiveLink href="/log">
+      <Button><Clipboard size="30%"/><span className="caption">View Log</span></Button>
+      </ActiveLink>
+    <ActiveLink href="/">
+      <Button><Clock size="30%"/><span className="caption">Transaction History</span></Button>
+      </ActiveLink>
   </div>
 ) 
