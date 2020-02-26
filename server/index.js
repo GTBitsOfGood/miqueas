9139f56981f17c7764541d34f3968e18e47d133d
Sho Szczepaniuk <sszczepaniuk3@gatech.edu>
2020-02-25 19:40:09 -0500

    Added config file, item actions,  item schema, and item api

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose';
+import config from '../../config';
 
 export default async () => {
   if (mongoose.connections[0].readyState) return;
 
   await mongoose
-    .connect('mongodb+srv://miqueas-dev:' + process.env.DB_PASS + '@cluster0-9lk2k.mongodb.net/test?retryWrites=true&w=majority', {
+    .connect(config.dbUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true,
-      dbName: urls.dbName
+      dbName: config.dbName
     })
     .catch(e => {
       console.error('Error connecting to database.');
