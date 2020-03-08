47925eee4068a256249aa0ae19286dc39d9a7f1d
srathi7 <srathi7@gatech.edu>
2020-03-08 16:22:39 -0400

    Made location buttons look like figma

--- frontend/components/TransactionForm.js
+++ frontend/components/TransactionForm.js
@@ -59,8 +59,7 @@ class TransactionForm extends React.Component {
           'blue', 'orange', 'green', 'purple', 'white', 'yellow',
           'black', 'pink', 'red', 'glitter']} />
         <hr/>
-        <HorizontalRadio name={'Location'}
-          options = {['bodega', 'downstairs']}/>
+        <LocationSelector></LocationSelector>
         <hr/>
         <NavButtons/>
       </div>
@@ -315,7 +314,7 @@ class VerticalRadio extends React.Component {
   }
 }
 
-class HorizontalRadio extends React.Component {
+class LocationSelector extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -323,38 +322,122 @@ class HorizontalRadio extends React.Component {
     };
   }
 
-  updateSelected(i) {
+  changeLocation(i) {
     this.setState({
       selected: i,
     });
+    // this.props.onClick(i);
   }
 
   render() {
+<<<<<<< Updated upstream
     const options = [];
     this.props.options.forEach((option, i) => {
       options.push(
           <Col key = {option + i.toString()}>
-            <Container>
-              <Row className="justify-content-center">
-                <Button
-                  variant={'outline-secondary'} size={'md'} block
-                  active = {this.state.selected == i}
-                  onClick={() => this.updateSelected(i)}>{option}</Button>
-              </Row>
-            </Container>
-          </Col>,
-      );
-    });
+=======
+    const bodegaBtnStyle = {'height': '64px',
+                            'fontSize': '14px',
+                            'textAlign': 'center'};
+    if (this.state.selected == 0) {
+      bodegaBtnStyle['background-color'] = '#51ADA9';
+      bodegaBtnStyle['color'] = 'white';
+    }
+    const bodegaBtn = (
+      <Col style = {{'paddingLeft': '0px', 'paddingRight': '5px'}}>
+        <Container>
+          <Row className="justify-content-center">
+            <Button variant={'outline-secondary'} size={'sm'} block
+              active = {this.state.selected == 0}
+              onClick={() => this.changeLocation(0)}
+              style={bodegaBtnStyle}>bodega
+              </Button>
+          </Row>
+        </Container>
+      </Col>);
+
+    const downBtnStyle = {'height': '64px',
+                          'fontSize': '14px',
+                          'textAlign': 'center'};
+    if (this.state.selected == 1) {
+      downBtnStyle['background-color'] = '#51ADA9';
+      downBtnStyle['color'] = 'white';
+    }
+    const downstairsBtn = (
+      <Col style = {{'paddingLeft': '5px', 'paddingRight': '0px'}}>
+        <Container>
+          <Row className="justify-content-center">
+            <Button variant={'outline-secondary'} size={'sm'} block
+              active = {this.state.selected == 1}
+              onClick={() => this.changeLocation(1)}
+              style={downBtnStyle}>downstairs
+            </Button>
+          </Row>
+        </Container>
+      </Col>
+    );
+
     return (
       <Container>
-        <p className = "text-muted">{this.props.name}</p>
         <Row className="justify-content-center">
-          {options}
+          <Col>
+            <p className = "text-muted">Location</p>
+          </Col>
+          <Col>
+>>>>>>> Stashed changes
+            <Container>
+              <Row className="justify-content-center">
+                {bodegaBtn}
+                {downstairsBtn}
+              </Row>
+            </Container>
+          </Col>
         </Row>
       </Container>
     );
   }
 }
+
+// class HorizontalRadio extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       selected: -1,
+//     };
+//   }
+
+//   updateSelected(i) {
+//     this.setState({
+//       selected: i,
+//     });
+//   }
+
+//   render() {
+//     const options = [];
+//     this.props.options.forEach((option, i) => {
+//       options.push(
+//           <Col>
+//             <Container>
+//               <Row className="justify-content-center">
+//                 <Button
+//                   variant={'outline-secondary'} size={'md'} block
+//                   active = {this.state.selected == i}
+//                   onClick={() => this.updateSelected(i)}>{option}</Button>
+//               </Row>
+//             </Container>
+//           </Col>,
+//       );
+//     });
+//     return (
+//       <Container>
+//         <p className = "text-muted">{this.props.name}</p>
+//         <Row className="justify-content-center">
+//           {options}
+//         </Row>
+//       </Container>
+//     );
+//   }
+// }
 
 class NavButtons extends React.Component {
   render() {
