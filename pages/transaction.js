c74a9264e365effd0af77d0c24ead89b142f9db8
Sukhmai <sukhmai.kapur@gmail.com>
2020-03-27 21:36:36 -0400

    fixed build errors with default transaction

--- pages/transaction.js
+++ pages/transaction.js
@@ -5,14 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Transaction({language, transactionState, setTransactionState}) {
   let items = transactionState.transactionItems;
+  let recentItem = items[items.length - 1];
+  
   return (
     <div>
       <TransactionForm
         language={language}
         transactionState={transactionState}
         setTransactionState={setTransactionState}
-        name={items[items.length-1].item.name}
-        category={items[items.length - 1].item.category}
+        name={recentItem ? recentItem.item.name : ""}
+        category={recentItem ? recentItem.item.category : ""}
       />
     </div>
   );
