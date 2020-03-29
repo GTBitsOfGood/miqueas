6eee4e2892611e9829237552856f74532ae5e300
Sho Szczepaniuk <sszczepaniuk3@gatech.edu>
2020-03-29 15:19:55 -0400

    categoryPage done

--- frontend/actions/Items.js
+++ frontend/actions/Items.js
@@ -93,3 +93,20 @@ export const getItemVariation = async (name) => fetch(
     }
     return json.payload;
   });
+
+  export const getCategories = async () => fetch(
+    config.baseUrl + config.apis.getCategories, {
+      method: 'get',
+      mode: 'same-origin',
+      credentials: 'include',
+    },
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      if (json == null) {
+        throw new Error('Could not connect to API!');
+      } else if (!json.success) {
+        throw new Error(json.message);
+      }
+      return json.payload;
+    });
